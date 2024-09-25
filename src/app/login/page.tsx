7624a27be9115963f935c35363d3e6122916be35
ABINASH A. <allesabinash1@gmail.com>
2024-09-25 06:32:34 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-red-600 text-2xl">Login</h1>
+     
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUpPage() {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-red-600 text-2xl">Sign Up</h1>
+      
+    </div>
+  );
+}
